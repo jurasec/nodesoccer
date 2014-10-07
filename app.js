8e4a6b27cb97bc26aed06c0f3d4dce8e03f0f833
Julio César Rodríguez Domínguez <jurasec@gmail.com>
2014-10-07 17:27:43 -0500

    adding new module

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = require('./libs/nodesoccer');
+var nodesoccer = require('./libs/nodesoccer');
 
 var app = express();
 
@@ -41,10 +41,10 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+        // res.render('error', {
+        //     message: err.message,
+        //     error: err
+        // });
     });
 }
 
--- libs/nodesoccer.js
+++ libs/nodesoccer.js
@@ -1,9 +1,19 @@
 /*
  * Nodesoccer.js
  */
+var five = require("johnny-five");
+var Spark = require("spark-io");
+var board = new five.Board({
+  io: new Spark({
+    token: "7550da40ccf9ac697211eb17c031bbe7ac16a20a",
+    deviceId: "53ff72066667574837422067"
+  })
+});
 
-var nodesoccer;
 
-console.log('testing module');
+board.on("ready", function() {
+  var led = new five.Led("D7");
+  led.blink();
+});
 
-module.exports = nodesoccer;
+
